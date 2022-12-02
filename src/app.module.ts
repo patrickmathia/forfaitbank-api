46c5902725e2765ec03c7b2044cfbb2d0af960d9
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-02 13:55:26 +0000

    created auth module and added class-validator dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-   imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+   imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      PrismaModule,
+      AuthModule,
+   ],
    controllers: [],
    providers: [],
 })
