4521c9c85030204222040795114656ed549b99b9
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-13 16:13:53 +0000

    cors policy update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  app.useGlobalPipes(
-    new ValidationPipe({
-       whitelist: true,
-    })
- );
+   const app = await NestFactory.create(AppModule);
+   await app.listen(3000);
+   app.useGlobalPipes(
+      new ValidationPipe({
+         whitelist: true,
+      })
+   );
+
+   app.enableCors({
+      origin: "*",
+      credentials: true,
+      allowedHeaders: "*",
+      optionsSuccessStatus: 200,
+   });
 }
 bootstrap();
