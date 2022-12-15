5e8a31031b9cef73cb9f5d136fa07fa2bde8d7c8
Patrick <99044724+pMattheew@users.noreply.github.com>
2022-12-15 15:44:07 +0000

    fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-   const app = await NestFactory.create(AppModule);
-   await app.listen(3000);
-   app.useGlobalPipes(
-      new ValidationPipe({
-         whitelist: true,
-      })
-   );
-
-   app.enableCors({
-      origin: "*",
-      credentials: true,
-      allowedHeaders: "*",
-      optionsSuccessStatus: 200,
-   });
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: "*",
+			credentials: true,
+			allowedHeaders: "*",
+			optionsSuccessStatus: 200,
+		},
+	});
+	await app.listen(3000);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+		})
+	);
 }
 bootstrap();
