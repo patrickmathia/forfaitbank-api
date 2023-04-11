a21e32cf2b278963a192552fddbd6f34122dca2d
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-04-11 16:04:48 -0300

    !feat(scripts): organization, refactoring, and dev. db fix for use in dev. environment (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
-import { Module } from "@nestjs/common";
-import { PrismaModule } from "./prisma/prisma.module";
-import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
-import { OperationModule } from './operation/operation.module';
-import { DatabaseModule } from './database/database.module';
+import { Module } from "@nestjs/common"
+import { PrismaModule } from "./prisma/prisma.module"
+import { ConfigModule } from "@nestjs/config"
+import { AuthModule } from "./auth/auth.module"
+import { UserModule } from "./user/user.module"
+import { OperationModule } from "./operation/operation.module"
+import { DatabaseModule } from "./database/database.module"
 
 @Module({
-   imports: [
-      ConfigModule.forRoot({ isGlobal: true }),
-      PrismaModule,
-      AuthModule,
-      UserModule,
-      OperationModule,
-      DatabaseModule,
-   ],
-   controllers: [],
-   providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === "test"
+          ? "./test/.env.test"
+          : "./prisma/.env.dev",
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    OperationModule,
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
